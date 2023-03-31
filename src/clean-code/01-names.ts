b2353465e806c0a7bcf4e8843ff6deca54bb2112
Oscar Galindez <ogalindezabreu@santandertecnologia.com.ar>
2023-03-31 15:21:56 -0300

    Chapter011: Se hace el refactor a nombres mas descriptivos

--- src/clean-code/01-names.ts
+++ src/clean-code/01-names.ts
@@ -2,19 +2,19 @@
 
     // Ejemplo
     // Archivos a evaluar - files to evaluate
-    const fs = [
-        { id: 1, f: false },
-        { id: 2, f: false },
-        { id: 3, f: true },
-        { id: 4, f: false },
-        { id: 5, f: false },
-        { id: 7, f: true },
+    const filesToEvaluate = [
+        { id: 1, delete: false },
+        { id: 2, delete: false },
+        { id: 3, delete: true },
+        { id: 4, delete: false },
+        { id: 5, delete: false },
+        { id: 7, delete: true },
     ]; 
     
     // Archivos marcados para borrar - files to delete
-    const arhivos = fs.map( f => f.f );
+    const filesToDelete = filesToEvaluate.map( eachFile => eachFile.delete );
 
-    
+    // Malos nombres
     class AbstractUser { };
     class UserMixin { };
     class UserImplementation { };
@@ -28,25 +28,25 @@
     // Todo: Tarea
         
     // día de hoy - today
-    const ddmmyyyy = new Date();
+    const currentDay = new Date();
     
     // días transcurridos - elapsed time in days
-    const d: number = 23;
+    const elapsedDays: number = 23;
     
     // número de archivos en un directorio - number of files in directory
-    const dir = 33;
+    const numberOfFilesInDirectory: number = 33;
     
     // primer nombre - first name
-    const nombre = 'Fernando';
+    const firstName = 'Fernando';
     
     // primer apellido - last name
-    const apellido = 'Herrera';
+    const lastName = 'Herrera';
 
     // días desde la última modificación - days since modification
-    const dsm = 12;
+    const daysSinceModification = 12;
     
     // cantidad máxima de clases por estudiante - max classes per student
-    const max = 6;
+    const maxClassesPerStudent = 6;
 
 
 })();
